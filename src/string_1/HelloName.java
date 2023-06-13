37d207727aa7fbda0b51e6322567a76aca6e6468
hasanertas <hsnertas@gmail.com>
2023-06-13 12:36:31 -0400

    First String

---
+++ src/string_1/HelloName.java
@@ -0,0 +1,18 @@
+package string_1;
+
+public class HelloName {
+
+public static void main(String [] args){
+
+System.out.println(helloName("hasan"));
+
+}
+
+
+public static String helloName(String name){
+
+    return "Hello "+ name+"!";
+}
+
+
+}
