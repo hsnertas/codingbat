23b5dc15de9277dabc3d08d2d7595b9ced09d3bc
hsnertas@gmail.com <hsnertas@gmail.com>
2023-06-19 12:58:22 -0400

    ExtrEnd added

---
+++ src/string_1/ExtraEnd.java
@@ -0,0 +1,9 @@
+package string_1;
+
+public class ExtraEnd {
+
+    public String extraEnd(String str) {
+
+        return str.substring(str.length()-2)+str.substring(str.length()-2)+str.substring(str.length()-2);
+    }
+}
