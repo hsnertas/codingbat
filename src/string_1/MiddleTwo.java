d8848faf4c23021c3d8fda22a1ebb70b933b1ba8
hsnertas@gmail.com <hsnertas@gmail.com>
2023-06-26 11:46:01 -0400

    MiddleTwo added

---
+++ src/string_1/MiddleTwo.java
@@ -0,0 +1,15 @@
+package string_1;
+
+public class MiddleTwo {
+
+    public String middleTwo(String str) {
+
+
+        if(str.length()%2==0){
+
+            return str.substring((str.length()/2)-1,(str.length()/2)+1 );
+        }else{
+            return str;
+        }
+    }
+}
