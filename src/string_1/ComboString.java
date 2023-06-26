b592af94bc1b45478605b906f2e7d31e78c5d632
hsnertas@gmail.com <hsnertas@gmail.com>
2023-06-26 11:30:21 -0400

    ComboString added

---
+++ src/string_1/ComboString.java
@@ -0,0 +1,16 @@
+package string_1;
+
+public class ComboString {
+
+    public String comboString(String a, String b) {
+
+        if(a.length()>b.length()){
+
+            return b+a+b;
+        }
+        else{
+            return a+b+a;
+        }
+
+    }
+}
